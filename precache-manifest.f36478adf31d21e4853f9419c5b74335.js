8250e993461960342ce038098f7d6c1862703f26
peterkariukimutuura <kariukipeter30@gmail.com>
2019-10-23 16:48:20 +0300

    Updates

---
+++ precache-manifest.f36478adf31d21e4853f9419c5b74335.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42aaac0d7c942c77a50b7399a7fec688",
+    "url": "/r/index.html"
+  },
+  {
+    "revision": "d637859a4574993d5bed",
+    "url": "/r/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0cf3b21937bc3ab4a416",
+    "url": "/r/static/js/2.181c5c75.chunk.js"
+  },
+  {
+    "revision": "d637859a4574993d5bed",
+    "url": "/r/static/js/main.d8fcd18e.chunk.js"
+  },
+  {
+    "revision": "4e5a53e574306b6b05cd",
+    "url": "/r/static/js/runtime-main.6fb438a0.js"
+  }
+]);
